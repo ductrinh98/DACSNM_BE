0451bf29ff657e98e2e5db0d48f2a78f314bf2e8
le duc trinh <trinhle0120@gmail.com>
2021-11-25 21:55:10 +0700

    fix cors

--- app.js
+++ app.js
@@ -22,17 +22,10 @@ db.once('open', function () {
     console.log('database connected!')
 });
 
-//fix cors
-app.use((req,res,next)=> {
-  res.header("Access-Control-Allow-Origin","*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Request-With, Content-Type, Accept, Authorization"
-  )
-  if(req.method ==='OPTIONS'){
-    res.header("Access-Control-Allow-Methods","PUT, POST, PATH, DELETE");
-    return res.status(200).json({});
-  }
+/* CROS middleware */
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
