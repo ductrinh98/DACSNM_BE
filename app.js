edf9f64c9856ad2b4063e1b5cf1040a9d913bf43
le duc trinh <trinhle0120@gmail.com>
2021-11-25 22:04:30 +0700

    fix cors

--- app.js
+++ app.js
@@ -23,11 +23,19 @@ db.once('open', function () {
 });
 
 /* CROS middleware */
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+const cors = require('cors');
+const whitelist = ['http://localhost:4200', 'http://example2.com'];
+const corsOptions = {
+  credentials: true, // This is important.
+  origin: (origin, callback) => {
+    if(whitelist.includes(origin))
+      return callback(null, true)
+
+      callback(new Error('Not allowed by CORS'));
+  }
+}
+
+app.use(cors(corsOptions));
 
 //use sessions for tracking logins
 app.use(session({
